d562343619c36a3c7de8b66da727bc8ba4e0048d
Carlos Lopes <carlos.lopes.pb@compasso.com.br>
2023-08-18 17:41:03 -0300

    footer change

--- src/pages/Footer/Footer.tsx
+++ src/pages/Footer/Footer.tsx
@@ -9,29 +9,34 @@ import TwitterIcon from "../../images/TwitterIcon.png";
 function Footer() {
   return (
     <div className="footer">
-      <div className="footer-primary">
-        <div className="logo-footer">
-          <img src={LogoImage2} alt="Logo Icon Footer" />
-          <Link to="/" style={{ textDecoration: "none", color: "#fff" }}>
-            <h3>FitMe</h3>
-          </Link>
-        </div>
-        <div className="info-footer">
-          <h5>About us</h5>
-          <h5>Delivery</h5>
-          <h5>Help & Support</h5>
-          <h5>T&C</h5>
-        </div>
-        <div className="contact-footer">
-          <h5>
-            Contact: <span>+91 1234567899</span>
-          </h5>
-        </div>
+      <div className="info-footer">
+        <h5>About us</h5>
+        <h5>Delivery</h5>
+        <h5>Help & Support</h5>
+        <h5>T&C</h5>
       </div>
-      <div className="footer-social-media">
-        <img src={FacebookIcon} alt="Facebook Icon" />
-        <img src={InstagramIcon} alt="Instagram Icon" />
-        <img src={TwitterIcon} alt="Twitter Icon" />
+      <div className="footer-content">
+        <div className="footer-dados">
+          <div className="logo-contact">
+            <div className="logo-footer">
+              <img src={LogoImage2} alt="Logo Icon Footer" />
+              <Link to="/" style={{ textDecoration: "none", color: "#fff" }}>
+                <h3 className="title">FitMe</h3>
+              </Link>
+            </div>
+            <div className="contact-footer">
+              <h5>
+                Contact: <span>+91 1234567899</span>
+              </h5>
+            </div>
+          </div>
+          <div className="footer-social-media">
+            <img src={FacebookIcon} alt="Facebook Icon" />
+            <img src={InstagramIcon} alt="Instagram Icon" />
+            <img src={TwitterIcon} alt="Twitter Icon" />
+          </div>
+          <div className="info-design"></div>
+        </div>
       </div>
     </div>
   );
